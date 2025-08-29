ee92fd2dc476598e812c65c9c05d09ab8ed616d9
fiojmnz <fijimenezal@est.utn.ac.cr>
2025-08-29 11:06:34 -0600

    Clase Reserva
    Se modifica la clase

---
+++ src/Reserva/Reserva.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import java.time.LocalDate;
+/**
+ *
+ * @author AsusVivobook
+ */
+public class Reserva {
+    private Cliente cliente;
+    private Vehiculo vehiculo;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private boolean confirmada;
+
+    public Reserva(Cliente cliente, Vehiculo vehiculo, LocalDate fechaInicio, LocalDate fechaFin) {
+        this.cliente = cliente;
+        this.vehiculo = vehiculo;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.confirmada = false;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+    
+    
+    
+
+    public void confirmar() { this.confirmada = true; }
+    public boolean isConfirmada() { return confirmada; }
+
+    public String mostrarInfo() {
+        return "Reserva: Cliente " + cliente.nombre +
+               " | Vehículo: " + vehiculo.getPlaca() +
+               " | Inicio: " + fechaInicio + " | Fin: " + fechaFin +
+               " | Confirmada: " + confirmada;
+    }
+}
