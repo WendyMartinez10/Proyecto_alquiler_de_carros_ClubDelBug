110543f5cb5ab965e818f94db1b57a61b5c212ba
Gipsy Aguirre L <giaguirrelo@est.utn.ac.cr>
2025-08-29 05:57:32 -1200

    Se crea la clase GestorEmpleados

--- src/Empleados/GestorEmpleados.java
+++ src/Empleados/GestorEmpleados.java
@@ -2,9 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package BaseDeDatos;
+package Empleados;
 
-import Clases.Empleado;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Random;
