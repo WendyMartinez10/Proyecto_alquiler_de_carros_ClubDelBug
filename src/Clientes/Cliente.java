16c9749fbb339b56acf5c22e597ed0adcf33550f
WENDY_MARTINEZ <wemartinezlo@est.utn.ac.cr>
2025-08-29 10:09:33 -0600

    Modifica

--- src/Clientes/Cliente.java
+++ src/Clientes/Cliente.java
@@ -2,39 +2,74 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Clientes;
+package Clases;
 
-import Personas.Persona;
+
 import java.time.LocalDate;
-
-
 /**
  *
- * @author USER
+ * @author wen
  */
-public abstract class Cliente extends Persona {
-   
-    private String licencia;
+public class Cliente extends Persona {
+    private String licenciaConducir;
 
-    public Cliente(String cedula, String nombre, LocalDate fechaNacimiento, String telefono, String correo, String licencia) {
+    public Cliente(String cedula, String nombre, LocalDate fechaNacimiento,
+                   String telefono, String correo, String licenciaConducir) {
         super(cedula, nombre, fechaNacimiento, telefono, correo);
-         if (licencia == null || licencia.isEmpty()) {
-            throw new IllegalArgumentException("No se puede registrar cliente sin número de licencia.");
-        }
-
-        this.licencia = licencia;
+        this.licenciaConducir = licenciaConducir;
     }
 
-    public String getLicencia() { 
-        return licencia; 
+    public String getCedula() {
+        return cedula;
     }
 
-    public void setLicencia(String licencia) {
-        this.licencia = licencia;
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
     }
-  @Override
-    public String toString() {
-        return "Persona{" + "cedula:" + cedula + ", nombre:" + nombre + ", fechaNacimiento:" + fechaNacimiento + ", telefono:" + telefono + ", correo:" + correo +", licencia=" + licencia +  '}';
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setLicenciaConducir(String licenciaConducir) {
+        this.licenciaConducir = licenciaConducir;
     }
     
+    
+
+    public String getLicenciaConducir() { return licenciaConducir; }
+
+    @Override
+    public String mostrarInfo() {
+        return "Cliente: " + nombre + " | Cédula: " + cedula +
+               " | Edad: " + getEdad() + " | Licencia: " + licenciaConducir;
+    }
 }
