d4b5f321ce1de0f756ebe595c77451d43efd82ac
fiojmnz <fijimenezal@est.utn.ac.cr>
2025-08-29 10:18:10 -0600

    Clase Empleado
    modificacion

---
+++ src/Empleados/Empleado.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import java.time.LocalDate;
+/**
+ *
+ * @author wen
+ */
+public class Empleado extends Persona {
+    private String puesto;
+    private double salario;
+
+    public Empleado(String cedula, String nombre, LocalDate fechaNacimiento,
+                    String telefono, String correo, String puesto, double salario) {
+        super(cedula, nombre, fechaNacimiento, telefono, correo);
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+
+    @Override
+    public String mostrarInfo() {
+        return "Empleado: " + nombre + " | Puesto: " + puesto + " | Salario: " + salario;
+    }
+}
---
+++ src/Empleados/GestorEmpleados.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BaseDeDatos;
+
+import Clases.Empleado;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author wen
+ */
+public class GestorEmpleados {
+     private ArrayList<Empleado> empleados = new ArrayList<>();
+
+    public GestorEmpleados() {
+          Empleado e1 = new Empleado(
+                "123456789",               // cédula
+                "Juan Pérez",              // nombre
+                LocalDate.of(1990, 5, 15),// fecha de nacimiento
+                "88881234",               // teléfono
+                "juan.perez@mail.com",    // correo
+                "Vendedor",               // puesto
+                500.0                     // salario
+            );
+
+          
+            empleados.add(e1);
+    }
+     
+     
+     
+
+    public void agregarEmpleado(Empleado e) throws Exception {
+        for (Empleado x : empleados) {
+            if (x.getCedula().equals(e.getCedula())) {
+                throw new Exception("Ya existe un empleado con esa cédula.");
+            }
+        }
+        if (e.getEdad() < 18) throw new Exception("El empleado debe ser mayor de 18 años.");
+        if (!Pattern.matches("^[A-Za-z0-9+_.-]+@(.+)$", e.getCorreo()))
+            throw new Exception("Correo inválido.");
+        if (!e.getTelefono().matches("\\d{8}"))
+            throw new Exception("Teléfono inválido, debe tener 8 dígitos.");
+
+        empleados.add(e);
+    }
+
+    public Empleado buscarEmpleado(String cedula) {
+        for (Empleado e : empleados) {
+            if (e.getCedula().equals(cedula)) return e;
+        }
+        return null;
+    }
+
+    public void actualizarEmpleado(String cedula, String telefono, String correo, String puesto) throws Exception {
+        Empleado e = buscarEmpleado(cedula);
+        if (e == null) throw new Exception("Empleado no encontrado.");
+        if (!telefono.matches("\\d{8}")) throw new Exception("Teléfono inválido.");
+        if (!Pattern.matches("^[A-Za-z0-9+_.-]+@(.+)$", correo))
+            throw new Exception("Correo inválido.");
+        e.setTelefono(telefono);
+        e.setCorreo(correo);
+        e.setPuesto(puesto);
+    }
+
+    public void eliminarEmpleado(String cedula) throws Exception {
+        Empleado e = buscarEmpleado(cedula);
+        if (e == null) throw new Exception("Empleado no encontrado.");
+        empleados.remove(e);
+    }
+    
+      public Empleado obtenerEmpleadoAleatorio() throws Exception {
+        if (empleados.isEmpty()) {
+            throw new Exception("No hay empleados registrados.");
+        }
+        Random rand = new Random();
+        return empleados.get(rand.nextInt(empleados.size()));
+    }
+
+    public ArrayList<Empleado> listarEmpleados() { return empleados; }
+}
